19accb745e3d615f2dc606e4ae009a7e599b3b92
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-21 01:05:41 -0700

    add machine GET/POST requests for create methods and their corresponding views

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -20,5 +20,24 @@ namespace Factory.Controllers
     {
       return View(_db.Machines.ToList());
     }
+
+    public ActionResult Create()
+    {
+    ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+    return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine, int EngineerId)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      if (EngineerId != 0)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId }); 
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
