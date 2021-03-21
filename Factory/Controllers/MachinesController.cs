5588f089d5c568b768b753fe8efc92c76bb1ad97
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-21 11:29:39 -0700

    add machine edit GET/POST requests with corresponding views

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -48,5 +48,24 @@ namespace Factory.Controllers
         .FirstOrDefault(machine => machine.MachineId == id); 
       return View(thisMachine);
     }
+
+    public ActionResult Edit(int id)
+    {
+      Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name") ;
+      return View(thisMachine);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Machine machine, int EngineerId)
+    {
+      if (EngineerId != 0)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId });
+      }
+      _db.Entry(machine).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
