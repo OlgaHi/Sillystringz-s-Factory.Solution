ee7b51740d6cd5602f92513c91ae051fa43e6eb3
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-21 00:35:09 -0700

    add engineer edit method and corresponding view

--- Factory/Controllers/EngineerController.cs
+++ Factory/Controllers/EngineerController.cs
@@ -43,5 +43,19 @@ namespace Factory.Controllers
       return View(thisEngineer);
     }
 
+    public ActionResult Edit(int id)
+    {
+    Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+    return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Engineer engineer)
+    {
+    _db.Entry(engineer).State = EntityState.Modified;
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+    }
+
   }
 }
