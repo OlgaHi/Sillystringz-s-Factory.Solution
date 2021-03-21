7fa7e7a59195736d91384349e76baaeb57f885c3
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-20 18:43:22 -0700

    add Initial db migration

--- Factory/Controllers/EngineerController.cs
+++ Factory/Controllers/EngineerController.cs
@@ -15,10 +15,6 @@ namespace Factory.Controllers
       _db = db;
     }
 
-    public ActionResult Index()
-    {
-      List<Engineer> model = _db.Engineers.ToList(); 
-      return View(model);
-    }
+    
   }
 }
