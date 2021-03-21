50c228ffbe7903db39467d530aabd1d905aea4d0
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-20 18:31:52 -0700

    add GET request to read the list of engineers

---
+++ Factory/Controllers/EngineerController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList(); 
+      return View(model);
+    }
+  }
+}
